d688ae8b07e94399dd23acaac026d76875e04a5d
Michael McClenaghan <mike@sideline.ca>
2011-02-22 14:53:09 -0700

    Reworked code to support the ability to read a master password from the DASHMASTER environment variable.

--- lib/dash.rb
+++ lib/dash.rb
@@ -10,12 +10,13 @@ module Dash
     else
       charset = STRONG
     end
-    master = args.shift.strip
     domain = args.shift.strip
+    master = (args.shift || ENV['DASHMASTER']).strip
 
     key = Digest::SHA2.hexdigest master
     password = Digest::SHA2.hexdigest "#{key}#{domain}"
-    puts "#{domain} has a dash of #{encode(password, charset)[0,12]}"
+
+    puts encode(password, charset)[0,12]
   end
 
   def self.encode(s,c);(t=s.unpack('C*').inject([0,'',0]){|a,v|a[0]==0?[2,a[1]+c[v>>2,1],v*16&48]:a[0]==2?[4,a[1]+c[v>>4|a[2],1],v*4&60]:[0,a[1]+c[v>>6|a[2],1]+c[v&63,1],0]})[1]+(t[0]==0?'':t[0]==2?c[t[2],1]+'==':c[t[2],1]+'=');end
--- lib/dash/version.rb
+++ lib/dash/version.rb
@@ -1,3 +1,3 @@
 module Dash
-  VERSION = "0.0.2"
+  VERSION = "0.1.1"
 end
