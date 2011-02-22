548d11a5bdd633791d69ea788a01db372ca2d99a
Michael McClenaghan <mike@sideline.ca>
2011-02-22 13:44:16 -0700

    Got the code working with a pretty output.

--- lib/dash.rb
+++ lib/dash.rb
@@ -8,7 +8,7 @@ module Dash
     domain = 'yahoo.com' #ARGV[0]
     key = Digest::SHA2.hexdigest 'cese962'
     password = Digest::SHA2.hexdigest "#{key}#{domain}"
-    encode(password, STRONG)
+    puts "#{domain} has a dash of #{encode(password, STRONG)[0,12]}"
   end
 
   def self.encode(s,c);(t=s.unpack('C*').inject([0,'',0]){|a,v|a[0]==0?[2,a[1]+c[v>>2,1],v*16&48]:a[0]==2?[4,a[1]+c[v>>4|a[2],1],v*4&60]:[0,a[1]+c[v>>6|a[2],1]+c[v&63,1],0]})[1]+(t[0]==0?'':t[0]==2?c[t[2],1]+'==':c[t[2],1]+'=');end
