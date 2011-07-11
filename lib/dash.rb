81752f4d6d3ca08bcb7e568657e9af46e4c74733
Michael McClenaghan <mike@sideline.ca>
2011-07-11 07:43:10 -0600

    Refactoring and added slop gem to manage options.

--- lib/dash.rb
+++ lib/dash.rb
@@ -1,23 +1,52 @@
 require 'digest/sha2'
+require 'slop'
 
 module Dash
+  extend self
   STRONG = 'ABCDEFGHJKLMNPQRSTUVWabcdefghijkmnopqrstuvw0123456789+/-_*()[]{}'
-  ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  ALPHA =  'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  LENGTH = 12
 
-  def self.hash(args)
-    if args.delete('--alpha')
-      charset = ALPHA
-    else
-      charset = STRONG
-    end
-    domain = args.shift.strip
-    master = (args.shift || ENV['DASHMASTER']).strip
+  def hash(args)
+    return unless parse_args(args)
+    
+    @key = encrypt @password
+    @hash = encrypt "#{@key}#{@domain}"
 
-    key = Digest::SHA2.hexdigest master
-    password = Digest::SHA2.hexdigest "#{key}#{domain}"
-
-    puts encode(password, charset)[0,12]
+    puts encode(@hash, @charset)[0,@length]
   end
 
-  def self.encode(s,c);(t=s.unpack('C*').inject([0,'',0]){|a,v|a[0]==0?[2,a[1]+c[v>>2,1],v*16&48]:a[0]==2?[4,a[1]+c[v>>4|a[2],1],v*4&60]:[0,a[1]+c[v>>6|a[2],1]+c[v&63,1],0]})[1]+(t[0]==0?'':t[0]==2?c[t[2],1]+'==':c[t[2],1]+'=');end
+  def parse_args(args)
+    opts = Slop.parse!(args) do
+      banner "Usage: dash <domain> [options]"
+      on :p, :password, 'Your master password', :argument => true
+      on :w, :weak, 'Generate weak password with alphanumeric character set'
+      on :l, :length, 'Length of generated password', :argument => true
+    end
+    
+    @charset = opts.weak? ? ALPHA : STRONG
+    @length = opts.length? ? opts[:length].to_i : LENGTH
+    
+    unless password = opts[:password] || ENV['DASH_PASSWORD']
+      puts opts.help
+      return
+    end
+    
+    unless domain = args.shift
+      puts opts.help
+      return
+    end
+    
+    @password = password.strip
+    @domain = domain.strip
+    true 
+  end
+  
+  def encode(s,c)
+    (t=s.unpack('C*').inject([0,'',0]){|a,v|a[0]==0?[2,a[1]+c[v>>2,1],v*16&48]:a[0]==2?[4,a[1]+c[v>>4|a[2],1],v*4&60]:[0,a[1]+c[v>>6|a[2],1]+c[v&63,1],0]})[1]+(t[0]==0?'':t[0]==2?c[t[2],1]+'==':c[t[2],1]+'=')
+  end
+  
+  def encrypt(key)
+    Digest::SHA2.hexdigest key
+  end
 end
--- lib/dash/version.rb
+++ lib/dash/version.rb
@@ -1,3 +1,3 @@
 module Dash
-  VERSION = "0.1.1"
+  VERSION = "0.2.0"
 end
