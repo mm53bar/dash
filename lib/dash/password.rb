1ed8fe1f97d06cb7f4bd7657962577a99b582a68
Michael McClenaghan <mike@sideline.ca>
2011-07-20 15:04:30 -0600

    Got the first test passing

--- lib/dash/password.rb
+++ lib/dash/password.rb
@@ -2,18 +2,16 @@ require 'digest/sha2'
 require 'slop'
 
 module Dash
-  extend self
+  class Password
   STRONG = 'ABCDEFGHJKLMNPQRSTUVWabcdefghijkmnopqrstuvw0123456789+/-_*()[]{}'
   WEAK =  'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   LENGTH = 12
 
-  def hash(args)
-    return unless parse_args(args)
+  def self.generate(domain, password, charset = STRONG, length = LENGTH)
+    key = encrypt password
+    hash = encrypt "#{key}#{domain}"
     
-    @key = encrypt @password
-    @hash = encrypt "#{@key}#{@domain}"
-
-    puts encode(@hash, @charset)[0,@length]
+    return encode(hash, charset)[0,length]
   end
 
   def parse_args(args)
@@ -42,11 +40,12 @@ module Dash
     true 
   end
   
-  def encode(s,c)
+  def self.encode(s,c)
     (t=s.unpack('C*').inject([0,'',0]){|a,v|a[0]==0?[2,a[1]+c[v>>2,1],v*16&48]:a[0]==2?[4,a[1]+c[v>>4|a[2],1],v*4&60]:[0,a[1]+c[v>>6|a[2],1]+c[v&63,1],0]})[1]+(t[0]==0?'':t[0]==2?c[t[2],1]+'==':c[t[2],1]+'=')
   end
   
-  def encrypt(key)
+  def self.encrypt(key)
     Digest::SHA2.new(256).hexdigest key
+  end
   end
 end
