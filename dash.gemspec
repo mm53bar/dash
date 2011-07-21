b8de756b8d4cd60d0c5fe8f973e43cc5d1d88ecf
Michael McClenaghan <mike@sideline.ca>
2011-07-20 19:03:41 -0600

    Dumped Thor and moved to Clamp gem

--- dash.gemspec
+++ dash.gemspec
@@ -19,8 +19,6 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
   
-  s.add_dependency('slop', '= 2.0.0')
+  s.add_dependency('clamp')
   s.add_development_dependency "rspec", "~> 2.6"
-  s.add_development_dependency "cucumber"
-  s.add_development_dependency "aruba"
 end
--- lib/dash/password.rb
+++ lib/dash/password.rb
@@ -1,51 +1,25 @@
 require 'digest/sha2'
-require 'slop'
 
 module Dash
   class Password
-  STRONG = 'ABCDEFGHJKLMNPQRSTUVWabcdefghijkmnopqrstuvw0123456789+/-_*()[]{}'
-  WEAK =  'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  LENGTH = 12
+    STRONG = 'ABCDEFGHJKLMNPQRSTUVWabcdefghijkmnopqrstuvw0123456789+/-_*()[]{}'
+    WEAK =  'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    LENGTH = 12
 
-  def self.generate(domain, password, charset = STRONG, length = LENGTH)
-    key = encrypt password
-    hash = encrypt "#{key}#{domain}"
+    def self.generate(domain, password, weak = false, length = LENGTH)
+      key = encrypt password
+      hash = encrypt "#{key}#{domain}"
+      charset = (weak == true) ? WEAK : STRONG
     
-    return encode(hash, charset)[0,length]
-  end
-
-  def parse_args(args)
-    opts = Slop.parse!(args) do
-      banner "Usage: dash <domain> [options]"
-      on :p, :password, 'Your master password', :argument => true
-      on :w, :weak, 'Generate weak password with alphanumeric character set'
-      on :l, :length, 'Length of generated password', :argument => true
+      return encode(hash, charset)[0,length]
     end
-    
-    @charset = opts.weak? ? WEAK : STRONG
-    @length = opts.length? ? opts[:length].to_i : LENGTH
-    
-    unless password = opts[:password] || ENV['DASH_PASSWORD']
-      puts opts.help
-      return
-    end
-    
-    unless domain = args.shift
-      puts opts.help
-      return
-    end
-    
-    @password = password.strip
-    @domain = domain.strip
-    true 
-  end
   
-  def self.encode(s,c)
-    (t=s.unpack('C*').inject([0,'',0]){|a,v|a[0]==0?[2,a[1]+c[v>>2,1],v*16&48]:a[0]==2?[4,a[1]+c[v>>4|a[2],1],v*4&60]:[0,a[1]+c[v>>6|a[2],1]+c[v&63,1],0]})[1]+(t[0]==0?'':t[0]==2?c[t[2],1]+'==':c[t[2],1]+'=')
-  end
+    def self.encode(s,c)
+      (t=s.unpack('C*').inject([0,'',0]){|a,v|a[0]==0?[2,a[1]+c[v>>2,1],v*16&48]:a[0]==2?[4,a[1]+c[v>>4|a[2],1],v*4&60]:[0,a[1]+c[v>>6|a[2],1]+c[v&63,1],0]})[1]+(t[0]==0?'':t[0]==2?c[t[2],1]+'==':c[t[2],1]+'=')
+    end
   
-  def self.encrypt(key)
-    Digest::SHA2.new(256).hexdigest key
-  end
+    def self.encrypt(key)
+      Digest::SHA2.new(256).hexdigest(key || '')
+    end
   end
 end
