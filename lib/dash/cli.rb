46593be8cbad6958bad37e25a11370963d3cbe5c
Michael McClenaghan <mike@sideline.ca>
2012-01-05 08:50:46 -0700

    Removed clamp gem and switched to optparse

--- lib/dash/cli.rb
+++ lib/dash/cli.rb
@@ -1,21 +1,42 @@
-require 'clamp'
+require 'optparse'
 require 'dash/password'
 
 module Dash
-  class CLI < Clamp::Command 
-    option ["-l", "--length"], "N", "make a password of N length", :default => 12 do |s|
-      Integer(s)
-    end
-    option ["-w", "--weak"], :flag, "generate a weak password"
-    parameter "DOMAIN", "the domain that needs a password", :attribute_name => :domain
-    parameter "[PASSWORD]", "your master password", :attribute_name => :password
+  class CLI
+    def run
+      options = {}
+      optparse = OptionParser.new do|opts|
+        # Set a banner, displayed at the top
+        # of the help screen.
+        opts.banner = "Usage: dash [options] DOMAIN"
 
-    def execute
-      puts Dash::Password.generate(domain, password, weak?, length)
-    end  
-    
-    def default_password
-      ENV['DASH_PASSWORD'] || ''
+        # Define the options, and what they do
+        options[:weak] = false
+        opts.on( '-w', '--weak', 'Generate a weak password' ) do
+          options[:weak] = true
+        end
+
+        options[:length] = 12
+        opts.on( '-l', '--length N', Integer, 'Make a password of N length' ) do |l|
+          options[:length] = l
+        end
+
+        options[:password] = ENV['DASH_PASSWORD']
+        opts.on( '-p', '--password PASSWORD', 'Your master PASSWORD' ) do |p|
+          options[:password] = p
+        end
+
+        # This displays the help screen, all programs are
+        # assumed to have this option.
+        opts.on( '-h', '--help', 'Display this screen' ) do
+          puts opts
+          exit
+        end
+      end      
+      
+      optparse.parse!
+      
+      puts Dash::Password.generate(ARGV.shift, options[:password], options[:weak], options[:length]) 
     end
   end
 end
--- lib/dash/version.rb
+++ lib/dash/version.rb
@@ -1,3 +1,3 @@
 module Dash
-  VERSION = "0.3.1"
+  VERSION = "0.4.0"
 end
