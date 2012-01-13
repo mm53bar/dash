189ff242e4cd15917dbe821c81b7ba81a4d7cb23
Michael McClenaghan <mike@sideline.ca>
2012-01-13 12:08:28 -0700

    Added rspec back in and wrote a few more specs

--- spec/dash_spec.rb
+++ spec/dash_spec.rb
@@ -1,7 +1,25 @@
 require 'dash/password'
 
 describe Dash::Password do
+  before :all do
+    @password = Dash::Password.generate('twitter.com','password')
+  end
+
   it "generates consistent password" do
-    Dash::Password.generate('twitter.com','password').should eql("NpPrP8E5QGJ5")
+    @password.should eql "NpPrP8E5QGJ5"
+  end
+
+  it "should generate a 12 character password by default" do
+    @password.length.should eql 12
+  end
+
+  it "should generate a different password if a different key is used" do
+    @new_password = Dash::Password.generate('twitter.com','new_password')
+    @new_password.should_not eql @password
+  end
+   
+  it "should reliably regenerate a password given the same key and domain" do
+    @new_password = Dash::Password.generate('twitter.com','password')
+    @new_password.should eql @password
   end
 end
